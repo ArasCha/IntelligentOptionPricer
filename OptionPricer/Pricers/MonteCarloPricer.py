66a8eb8adb26c8dfac6edbe17cd91b2ce8eafbff
ArasCha <araschaignepro@gmail.com>
2025-03-23 17:38:54 +0100

    [EDIT] Monte Carlo calculation to use stored payoff function

--- OptionPricer/Pricers/MonteCarloPricer.py
+++ OptionPricer/Pricers/MonteCarloPricer.py
@@ -2,6 +2,8 @@ from Option import Call, Put
 import numpy as np
 from Pricers.Pricer import Pricer
 
+from scipy.stats import norm
+from scipy.stats.qmc import Sobol
 
 
 
@@ -17,11 +19,5 @@ class MonteCarloPricer(Pricer):
         
         W_T = np.sqrt(self.T) * np.random.normal(size=nb_samples) # W_T \sim \sqrt{T}\mathcal{N}(0,1)
         S_T = self.S * np.exp( (self.r - self.q - 0.5 * self.sigma**2) * self.T + self.sigma * W_T) # vector of size nb_samples. Solution of the Black-Scholes EDS
-
-        if isinstance(self.instrument, Call):
-            payoff = np.maximum( S_T - self.K, 0) # np.maximum to compute the max for each element of the array. np.max computes the max of the array
         
-        elif isinstance(self.instrument, Put):
-            payoff = np.maximum( self.K - S_T, 0)
-        
-        return np.mean(payoff * np.exp(-self.r * self.T))
+        return np.mean(self.payoff(S_T) * np.exp(-self.r * self.T))
--- OptionPricer/main.py
+++ OptionPricer/main.py
@@ -1,4 +1,5 @@
 from Pricers.BlackScholesPricer import BlackScholesPricer
+from Pricers.MonteCarloPricer import MonteCarloPricer
 from Option import Call, Put
 
 
@@ -12,6 +13,9 @@ if __name__ == "__main__":
                     volatility= 0.2,
                     dividend= 0.04)
 
-    pricer = BlackScholesPricer(instrument)
-    price = pricer.calculate()
-    print(price)
+    pricer_black_scholes = BlackScholesPricer(instrument)
+    print("Black-Scholes: ", pricer_black_scholes.calculate())
+
+    pricer_monte_carlo = MonteCarloPricer(instrument)
+    nb_samples = 100_000
+    print("Monte Carlo", pricer_monte_carlo.calculate(nb_samples))
