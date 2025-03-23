9c4c27f4d42937660edbfa60fd4d186bc7609462
ArasCha <araschaignepro@gmail.com>
2025-03-24 00:23:35 +0100

    [ADD] function to plot volatility surface
    Missing a lot of data points, interpolation should be done

--- OptionPricer/VolatilitySurface.py
+++ OptionPricer/VolatilitySurface.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import numpy as np
 import pandas as pd
 from datetime import datetime
+import plotly.graph_objects as go
 
 
 def build_iv_surface(ticker: yf.ticker.Ticker):
@@ -37,3 +38,34 @@ def build_iv_surface(ticker: yf.ticker.Ticker):
 
     return pd.DataFrame(iv_surface_data, columns=["Strike", "Maturity", "IV"])
 
+
+def plot_iv_surface(iv_df: pd.DataFrame):
+    """
+    Builds implied volatility surface from a  plots it as a 3D surface using Plotly
+    Parameters:
+    - iv_df: pd.DataFrame of the volatility surface to plot
+    """
+    
+    assert not iv_df.empty, "No implied volatility data found. Cannot plot"
+
+    df_pivot = iv_df.pivot(index="Strike", columns="Maturity", values="IV") # pivot the DataFrame to extract fields domains
+
+    fig = go.Figure(data=[
+        go.Surface(
+            x = df_pivot.index.values, # strike
+            y = df_pivot.columns.values, # maturity in years
+            z = df_pivot.values # implied vol
+        )
+    ])
+
+    fig.update_layout(
+        title="Implied Volatility Surface",
+        scene=dict(
+            xaxis_title="Strike",
+            yaxis_title="Time to Maturity (Years)",
+            zaxis_title="Implied Volatility"
+        ),
+        autosize=True
+    )
+
+    fig.show()
