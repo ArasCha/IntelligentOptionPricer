e28d54a177c80d155c961c77d6886b28491298bc
ArasCha <araschaignepro@gmail.com>
2025-03-23 17:48:02 +0100

    [ADD] Monte Carlo pricer method to calculate with antithetic variables

--- OptionPricer/Pricers/MonteCarloPricer.py
+++ OptionPricer/Pricers/MonteCarloPricer.py
@@ -39,3 +39,18 @@ class MonteCarloPricer(Pricer):
         S_T = self.S * np.exp( (self.r - self.q - 0.5 * self.sigma**2) * self.T + self.sigma * W_T) # vector of size nb_samples. Solution of the Black-Scholes EDS
         
         return np.mean(self.payoff(S_T) * np.exp(-self.r * self.T))
+
+
+    def calculate_antithetic(self, nb_samples: int) -> float:
+        """
+        Calculate the price of the option with antithetic variables
+        """
+        
+        W_T = np.sqrt(self.T) * np.random.normal(size=nb_samples)
+        drift = (self.r - self.q - 0.5 * self.sigma**2) * self.T
+        S_T1 = self.S * np.exp( drift + self.sigma * W_T)
+        S_T2 = self.S * np.exp( drift + self.sigma * -W_T) # antithetic of S_T1
+
+        payoff = ( self.payoff(S_T1) + self.payoff(S_T2) )/2
+
+        return np.mean(payoff * np.exp(-self.r * self.T))
