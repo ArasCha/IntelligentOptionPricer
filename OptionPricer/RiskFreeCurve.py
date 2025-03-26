5ef1c168430b49a440510fca085ea397fb803adf
ArasCha <araschaignepro@gmail.com>
2025-03-26 08:38:30 +0100

    [ADD] method to get latest rates

--- OptionPricer/RiskFreeCurve.py
+++ OptionPricer/RiskFreeCurve.py
@@ -1,5 +1,6 @@
 
 import yfinance as yf
+import numpy as np
 
 class MarketDataFetcher:
     """
@@ -30,6 +31,34 @@ class MarketDataFetcher:
         else:
             raise ValueError("No data retrieved for the specified period.")
 
+    
+    def get_latest_rates(self):
+        """
+        Extracts the latest interest rate points (in percentage) and converts them to decimal.
+        :return: (maturities, rates) as (np.array, np.array).
+        """
+        if self._data is None:
+            raise ValueError("You must call fetch_data() first.")
+
+        # Select the DataFrame with closing prices
+        adj_close = self._data['Close'].copy()
+
+        # Retrieve the rates (in %) at the last date
+        last_rates_percent = adj_close.loc[self._last_date, self.tickers]
+
+        # Convert to decimal
+        last_rates_decimal = last_rates_percent / 100.0
+
+        # Define our maturities (approx.) in years
+        # Corresponding to tickers: ^IRX (3 months), ^FVX (5 years), ^TNX (10 years), ^TYX (30 years)
+        maturities = np.array([0.25, 5.0, 10.0, 30.0])
+        rates = last_rates_decimal.values
+
+        return maturities, rates
+
+    @property
+    def last_date(self):
+        return self._last_date
 
 
 tickers = ["^IRX", "^FVX", "^TNX", "^TYX"]  # 3 mois, 5 ans, 10, 30
@@ -37,5 +66,6 @@ start_date = "2023-01-01"
 end_date = "2023-03-01"
 
 datafetcher = MarketDataFetcher(tickers, start_date, end_date)
-print(datafetcher._data)
+print(datafetcher.get_latest_rates())
+print(datafetcher.last_date)
 
