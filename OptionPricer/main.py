e394da1b14a916bf7cb493a574551a73ae3995c0
ArasCha <araschaignepro@gmail.com>
2025-03-28 19:08:57 +0100

    [DEL] deletion of features not used for pricing

--- OptionPricer/main.py
+++ OptionPricer/main.py
@@ -2,8 +2,6 @@ from Pricers.BlackScholesPricer import BlackScholesPricer
 from Pricers.MonteCarloPricer import MonteCarloPricerAntithetic, MonteCarloPricerClassic, MonteCarloPricerLazy, MonteCarloPricerQMC
 from Pricers.Pricer import Pricer
 from Option import Call, Put
-from VolatilitySurface import ImpliedVolSurfaceBuilder
-from RiskFreeCurve.CurveInterface import CurveInterface
 
 
 
@@ -16,19 +14,6 @@ if __name__ == "__main__":
                     volatility= 0.2,
                     dividend= 0.04)
 
-    # pricer_black_scholes = BlackScholesPricer(instrument)
-    # print("Black-Scholes: ", pricer_black_scholes.calculate())
-
-    # pricer_monte_carlo = MonteCarloPricer(instrument)
-    # nb_samples = 2**18 # power of 2 for Sobol sequence in qmc
-    # print("Monte Carlo calculate", pricer_monte_carlo.calculate(nb_samples))
-    # print("Monte Carlo calculate_qmc", pricer_monte_carlo.calculate_qmc(nb_samples))
-    # print("Monte Carlo calculate_antithetc", pricer_monte_carlo.calculate_antithetic(nb_samples))
-    # print("Monte Carlo calculate_lazy", pricer_monte_carlo.calculate_lazy(nb_samples))
-
-    # print(pricer_monte_carlo.benchmark(10000))
-
-    # ImpliedVolSurfaceBuilder("AAPL").plot_surface()
 
     pricers: list[Pricer] = [
         MonteCarloPricerQMC(instrument),
@@ -46,15 +31,3 @@ if __name__ == "__main__":
             "price": price
         }
     print(results)
-        
-
-
-
-    # tickers = ["^IRX", "^FVX", "^TNX", "^TYX"]  # 3 mois, 5 ans, 10, 30
-    # start_date = "2023-01-01"
-    # end_date = "2023-03-01"
-
-    # curve = CurveInterface(tickers, start_date, end_date)
-    # for plot in [0.25, 1, 2, 5, 10, 20, 30]:
-    #     discount_factor, zero_rate = curve.infer(plot)
-    #     print(f"Maturity {plot:5.2f} yrs | DF = {discount_factor:6.4f} | ZeroRate cont. = {zero_rate * 100:4.2f}%")
